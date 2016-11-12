6b0afbf8fa8e8a5fe10e35dcd52a8e13e367e13c
Steffi Indrayani <Steffi Indrayani>
2016-11-13 05:34:59 +0700

    upload confirm purchase web client, add confirm purchase server

--- Marketplace/src/java/Marketplace/service.java
+++ Marketplace/src/java/Marketplace/service.java
@@ -5,38 +5,34 @@
  */
 package Marketplace;
 
-import connection.connectDB;
-import item.item;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import connection.*;
+import item.*;
+import javax.ejb.Stateless;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
+import java.util.ArrayList;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebParam;
-import javax.ejb.Stateless;
+import user.user;
 
 /**
  *
  * @author vitra
  */
-@WebService(serviceName = "service")
 @Stateless()
+@WebService(serviceName = "Service")
 public class service {
 
     /**
      * This is a sample web service operation
      * @param idus
-     * @return 
+     * @return
      */
-    @WebMethod(operationName = "yourProduct")
-    public ArrayList<item> yourProduct(@WebParam(name = "id_user") String idus) {
+    @WebMethod(operationName = "array")
+    public ArrayList<item> arrayProduct(@WebParam(name = "idus")String idus) {
         ArrayList<item> list = new ArrayList();
         
         connectDB connDB = new connectDB();
@@ -89,132 +85,114 @@ public class service {
         connDB.closeConn();
         return list;
     }
-    
-    /**
-     * This is a sample web service operation
-     * @param idus
-     * @return 
-     */
-    @WebMethod(operationName = "viewCatalog")
-    public ArrayList<item> viewCatalog(@WebParam(name = "id_user") String idus) {
-        ArrayList<item> list = new ArrayList();
+    @WebMethod(operationName = "menunjukkan")
+    public item showproductinfo(@WebParam(name = "id_product") String id_product) {
+        //TODO write your implementation code here:
         
         connectDB connDB = new connectDB();
         Connection conn = connDB.connectMP();
         
-        PreparedStatement sql;
+        item x = new item(0,"","","","",0,"",0,0);
+        
+        PreparedStatement sql; 
         try {
-            sql = conn.prepareStatement("SELECT id_product, name, price, description, date_added, time_added, "
-                                        + "seller_id, photo, username, deleted FROM product, user "
-                                        + "WHERE id_user = seller_id "
-                                        + "ORDER BY date_added DESC, time_added DESC");
-            ResultSet result = sql.executeQuery();
-            while (result.next()) {
-                Boolean deleted = result.getBoolean("deleted");
-                if (!deleted) {
-                    int idpro = result.getInt("id_product");
-
-                    Date phpdate = result.getDate("date_added");
-                    SimpleDateFormat df = new SimpleDateFormat("d F Y");
-                    String date = df.format(phpdate);
-
-                    Time phptime = result.getTime("time_added");
-                    SimpleDateFormat tf = new SimpleDateFormat("h");
-                    String time = tf.format(phptime);
-
-                    String photo = result.getString("photo");
-                    String name = result.getString("name");
-                    int price = result.getInt("price");
-                    //$price = number_format($phpprice, 0, ',', '.');
-                    String desc = result.getString("description");
-
-                    PreparedStatement sql1 = conn.prepareStatement("SELECT count(id_product) FROM likes WHERE id_product = ?"); 
-                    sql1.setString(1, Integer.toString(idpro));
-                    ResultSet result1 = sql1.executeQuery();
-                    int likes = 0; //result1.getInt("count(id_product)");
-
-                    PreparedStatement sql2 = conn.prepareStatement("SELECT sum(quantity) FROM purchase WHERE id_product = ?"); 
-                    sql2.setString(1, Integer.toString(idpro));
-                    ResultSet result2 = sql2.executeQuery();
-                    int purch = 0; //result2.getInt("sum(quantity)");
-
-                    item x = new item(idpro, date, time, photo, name, price, desc, likes, purch);
-                    list.add(x);
-                }
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(service.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        connDB.closeConn();
-        return list;
-    }
-    
-    /**
-     * This is a sample web service operation
-     * @return 
-     */
-    @WebMethod(operationName = "viewCatalogSearch")
-    public ArrayList<item> viewCatalogSearch(@WebParam(name = "id_user") String idus, @WebParam(name = "search") String search, @WebParam(name = "search_by") String search_by) {
-        ArrayList<item> list = new ArrayList();
-        
-        connectDB connDB = new connectDB();
-        Connection conn = connDB.connectMP();
-        
-        PreparedStatement sql;
-        try {
-            if (search_by == "product") {
-                sql = conn.prepareStatement("SELECT id_product, name, price, description, date_added, time_added, "
-                                        + "seller_id, photo, username, deleted FROM product, user "
-                                        + "WHERE id_user = seller_id AND name like '%?%' "
-                                        + "ORDER BY date_added DESC, time_added DESC");
-            } else {
-                sql = conn.prepareStatement("SELECT id_product, name, price, description, date_added, time_added, "
-                                        + "seller_id, photo, username, deleted FROM product, user "
-                                        + "WHERE id_user = seller_id AND username like '%?%' "
-                                        + "ORDER BY date_added DESC, time_added DESC");
-            }
-            sql.setString(1, search);
+            sql = conn.prepareStatement("SELECT * FROM product WHERE id_product = ?");
+            sql.setString(1, id_product);
             
             ResultSet result = sql.executeQuery();
-            while (result.next()) {
-                Boolean deleted = result.getBoolean("deleted");
-                if (!deleted) {
-                    int idpro = result.getInt("id_product");
-
-                    Date phpdate = result.getDate("date_added");
-                    SimpleDateFormat df = new SimpleDateFormat("d F Y");
-                    String date = df.format(phpdate);
-
-                    Time phptime = result.getTime("time_added");
-                    SimpleDateFormat tf = new SimpleDateFormat("h");
-                    String time = tf.format(phptime);
-
-                    String photo = result.getString("photo");
-                    String name = result.getString("name");
-                    int price = result.getInt("price");
-                    //$price = number_format($phpprice, 0, ',', '.');
-                    String desc = result.getString("description");
-
-                    PreparedStatement sql1 = conn.prepareStatement("SELECT count(id_product) FROM likes WHERE id_product = ?"); 
-                    sql1.setString(1, Integer.toString(idpro));
-                    ResultSet result1 = sql1.executeQuery();
-                    int likes = 0; //result1.getInt("count(id_product)");
-
-                    PreparedStatement sql2 = conn.prepareStatement("SELECT sum(quantity) FROM purchase WHERE id_product = ?"); 
-                    sql2.setString(1, Integer.toString(idpro));
-                    ResultSet result2 = sql2.executeQuery();
-                    int purch = 0; //result2.getInt("sum(quantity)");
-
-                    item x = new item(idpro, date, time, photo, name, price, desc, likes, purch);
-                    list.add(x);
-                }
-            }
+            System.out.println("result.next() " + result.next());
+            String name = result.getString("name");
+            System.out.println("name  = "+ name);
+            int price = result.getInt("price");
+            int idpro = result.getInt("id_product");
+            x.setIdpro(idpro);
+            x.setName(name);
+            x.setPrice(price);
+            System.out.println("id = "+ x.getIdpro());
+            System.out.println("name = "+ x.getName());
+            System.out.println("price = "+ x.getPrice());
         } catch (SQLException ex) {
             Logger.getLogger(service.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
         connDB.closeConn();
-        return list;
+        return x;
     }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "showbuyer")
+    public user showbuyerinfo(@WebParam(name = "id_user") int id_user) {
+        //TODO write your implementation code here:
+                
+        connectDB connDB = new connectDB();
+        Connection conn = connDB.connectIS();
+        
+        user x = new user();
+        
+        PreparedStatement sql; 
+        try {
+            sql = conn.prepareStatement("SELECT * FROM user WHERE id_user = ?");
+            sql.setInt(1, id_user);
+            
+            ResultSet result = sql.executeQuery();
+            System.out.println("result.next() " + result.next());
+            String name = result.getString("full_name");
+            int idus = result.getInt("id_user");
+            String address = result.getString("full_address");
+            String postcode = result.getString("postal_code");       
+            String phone = result.getString("phone_number");
+            
+            x.setId(idus);
+            x.setFName(name);
+            x.setAddress(address);
+            x.setPostalcode(postcode);
+            x.setPhonenumber(phone);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(service.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        connDB.closeConn();
+        return x;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "purchase")
+    public int insertpurchase(@WebParam(name = "id_product") int id_product, @WebParam(name = "id_buyer") int id_buyer, @WebParam(name = "prodname") String prodname, @WebParam(name = "price") int price, @WebParam(name = "quantity") int quantity, @WebParam(name = "total_price") int total_price, @WebParam(name = "consignee") String consignee, @WebParam(name = "full_address") String full_address, @WebParam(name = "postal_code") String postal_code, @WebParam(name = "phone_number") String phone_number, @WebParam(name = "ccn") String ccn, @WebParam(name = "cvv") String cvv) {
+        //TODO write your implementation code here:
+        connectDB connDB = new connectDB();
+        Connection conn = connDB.connectMP();
+        
+        int i = 0;
+        System.out.println("database");
+        PreparedStatement sql; 
+        try {
+            sql = conn.prepareStatement("INSERT INTO PURCHASE (id_product, id_buyer, product_name, price, quantity, total_price, consignee, full_address, postal_code, phone_number, credit_card_number, card_verification_value, date_purchased, time_purchased) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,CURDATE(),CURTIME())");
+            sql.setInt(1, id_product);
+            sql.setInt(2, id_buyer);
+            sql.setString(3, prodname);
+            sql.setInt(4, price);
+            sql.setInt(5, quantity);
+            sql.setInt(6, total_price);
+            sql.setString(7, consignee);
+            sql.setString(8, full_address);
+            sql.setString(9, postal_code);
+            sql.setString(10, phone_number);
+            sql.setString(11, ccn);
+            sql.setString(12, cvv);
+
+
+            i = sql.executeUpdate();
+            
+           
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(service.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        connDB.closeConn();
+        return i;
+    }
+    
 }
---
+++ Marketplace/src/java/user/user.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user;
+
+/**
+ *
+ * @author User
+ */
+public class user {
+    private int id_user;
+    private String full_name;
+    private String username;
+    private String password;
+    private String full_address;
+    private String postal_code;
+    private String phone_number;
+    private String email;
+    
+/*    public user(int id_user, String full_name, String username, String password, String full_address, String postal_code, String phone_number, String email) {
+        this.id_user = id_user;
+        this.full_name = full_name;
+        this.username = username;
+        this.password = password;
+        this.full_address = full_address;
+        this.postal_code = postal_code;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+*/
+    public user() {
+        id_user = 0;
+    }
+    public int getId() {
+        return id_user;
+    }
+    
+    public void setId (int id_user) {
+        this.id_user = id_user;
+    }
+    
+    public String getFName() {
+        return full_name;
+    }
+    
+    public void setFName (String full_name) {
+        this.full_name = full_name;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername (String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword (String password) {
+        this.password = password;
+    }
+    
+    public String getAddress() {
+        return full_address;
+    }
+    
+    public void setAddress (String full_address) {
+        this.password = full_address;
+    }
+    
+    public String getPostalcode() {
+        return postal_code;
+    }
+    
+    public void setPostalcode (String postal_code) {
+        this.postal_code = postal_code;
+    }
+    
+    public String getPhonenumber() {
+        return phone_number;
+    }
+    
+    public void setPhonenumber (String phone_number) {
+        this.phone_number = phone_number;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail (String email) {
+        this.email = email;
+    }
+}
